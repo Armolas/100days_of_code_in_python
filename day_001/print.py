27762fa75734f913db8704516118d684aa78ec95
Armolas <armolas06@gmail.com>
2023-11-21 21:38:21 +0000

    day 1

---
+++ day_001/print.py
@@ -0,0 +1,9 @@
+print("Hello" + " Olasunkanmi")
+print("\n-------------------\n")
+print("Your favorite thing to do is " + 
+      input("what's your favorite thing to do? ") + "\n")
+print("\n-------------------\n")
+height = input("What is your height in cm? ")
+print("\n-------------------\n")
+print("You are " + height +"cm tall")
+print("This\nis\na\nmulti-line\nstatement")
