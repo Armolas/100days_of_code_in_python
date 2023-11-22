8aa7fc8595fed182fb7db57973ee43182172166d
Armolas <armolas06@gmail.com>
2023-11-22 06:47:23 +0000

    added band_name_gen.py

---
+++ day_001/band_name_gen.py
@@ -0,0 +1,5 @@
+print('Welcome To Band Name Generator!')
+city = input("What city did you grow up in?\n")
+pet_name = input("What is the name of your pet?\n")
+band_name = city + " " + pet_name
+print('Your band name could be "' + band_name + '"!')
